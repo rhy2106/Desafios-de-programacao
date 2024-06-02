c5b3ff0b814e4fe2195d7080c15e0f7f34e102c4
rhy2106 <rafaelbob133@gmail.com>
2024-06-02 06:02:21 -0300

    aaaa

--- src/1907.cpp
+++ src/1907.cpp
@@ -1,61 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-vector<vector<int>> grafo;
-vector<int> visitados;
 vector<vector<char>> matriz;
+vector<vector<bool>> visitados;
 int N, M;
 
-void dfs(int rotulo){
-    if(visitados[rotulo] == 0){
-        visitados[rotulo] = 1;
-        int linha = rotulo/M;
-        int coluna = rotulo%M;
-        if(matriz[linha][coluna] == '.'){
-            for(int i = 0; i < grafo[rotulo].size(); i++){
-                dfs(grafo[rotulo][i]);
+void bfs(int startX, int startY) {
+    queue<pair<int, int>> q;
+    q.push({startX, startY});
+    visitados[startX][startY] = true;
+
+    // Definindo os movimentos possíveis (cima, baixo, esquerda, direita)
+    vector<pair<int, int>> direcoes = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (auto [dx, dy] : direcoes) {
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && matriz[nx][ny] == '.' && !visitados[nx][ny]) {
+                visitados[nx][ny] = true;
+                q.push({nx, ny});
             }
         }
-     }
+    }
 }
+
 int main() {
-    scanf("%d %d", &N, &M);
-    grafo.clear();
-    grafo.resize(N*M);
-    visitados.clear();
-    visitados.resize(N*M,0);
-    matriz.clear();
-    matriz.resize(N);
+    cin >> N >> M;
+    matriz.resize(N, vector<char>(M));
+    visitados.resize(N, vector<bool>(M, false));
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> matriz[i][j];
+        }
+    }
+    
     int cont = 0;
-    for(int i = 0; i < N*M; i++){
-        if(i%M != 0){
-            grafo[i].push_back(i-1);
-        }
-        if(i%M != M-1){
-            grafo[i].push_back(i+1);
-        }
-        if(i/M != 0){
-            grafo[i].push_back(i-M);
-        }
-        if(i/M != N-1){
-            grafo[i].push_back(i+M);
+    
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (matriz[i][j] == '.' && !visitados[i][j]) {
+                cont++;
+                bfs(i, j);
+            }
         }
     }
-    for(int i = 0; i < N; i++){
-        string tmp;
-        cin >> tmp;
-        for(int j = 0; j < M; j++){
-            matriz[i].push_back(tmp[j]);
-        }
-    }
-    for(int i = 0; i < grafo.size(); i++){
-        if(visitados[i] == 0 && matriz[i/M][i%M] == '.'){
-            cont += 1;
-            dfs(i);
-        }
-    }
-    printf("%d\n", cont);
+    
+    cout << cont << endl;
     return 0;
 }
